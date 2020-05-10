49199a054ce7c93ff209982bcd04291ae18a552a
707 <707472783@qq.com>
2020-05-10 23:01:16 +0800

    add ssh demo

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use ssh2::Session;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
+fn main() {
+
+    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+
+    sess.userauth_password("username", "password").unwrap();
+    assert!(sess.authenticated());
+
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("{}", s);
+    channel.wait_close();
+    println!("{}", channel.exit_status().unwrap());
+}
